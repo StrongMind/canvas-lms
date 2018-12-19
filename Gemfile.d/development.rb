3da8217e0c829400cd0a7b82c5ca164d8fa64fdb
Chris Young <cyoung1664@fs1548mbpro.avaflipswitch.com>
2018-12-19 11:20:32 -0700

    run gem from local source

--- Gemfile.d/development.rb
+++ Gemfile.d/development.rb
@@ -21,11 +21,11 @@ group :development do
   gem 'spring', '2.0.2'
   gem 'spring-commands-rspec', '1.0.4'
   gem 'awesome_print'
+  gem 'canvas_shim', path: '/usr/src/canvas-lms/gems/canvas_shim/'
   # Option to DISABLE_RUBY_DEBUGGING is helpful IDE-based debugging.
   # The ruby debug gems conflict with the IDE-based debugger gem.
   # Set this option in your dev environment to disable.
   unless ENV['DISABLE_RUBY_DEBUGGING']
     gem 'byebug', '9.0.6', platform: :mri
-    
   end
 end
