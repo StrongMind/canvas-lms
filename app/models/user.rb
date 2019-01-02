5b4d6da0b291cdafa06e4174d31e842e71f41190
John VanBuskirk <tesla+john@strongmind.com>
2019-01-02 14:56:21 -0700

    mix in some logic to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -153,17 +153,6 @@ class User < ActiveRecord::Base
     all_conversations.visible.order("last_message_at DESC, conversation_id DESC")
   end
 
-  def get_teacher_unread_discussion_topics
-    topic_microservice_endpoint = ENV['TOPIC_MICROSERVICE_ENDPOINT']
-    api_key = ENV['TOPIC_MICROSERVICE_API_KEY']
-    return {} unless topic_microservice_endpoint && api_key
-    return {} unless self.enrollments.where(type: 'TecherEnrollment')
-      endpoint = "#{topic_microservice_endpoint}/teachers/#{ENV['CANVAS_DOMAIN']}:#{self.id}/topics/"
-      ret = HTTParty.get(endpoint, headers: { "x-api-key": api_key })
-      ret.body
-  end
-
-
   def page_views(options={})
     PageView.for_user(self, options)
   end
@@ -3015,3 +3004,4 @@ class User < ActiveRecord::Base
     roles
   end
 end
+require File.expand_path('../../app/models/user', CanvasShim::Engine.called_from)
