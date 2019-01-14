b1e0a135941bbd3c9ddedcbac861997bad8dddd3
Andy Rosenberg, Elle Bayless, and Mark Ng <tesla+andy+elle+mark@strongmind.com>
2019-01-14 14:19:02 -0700

    lockout includes primavera ms

--- lib/authentication_methods.rb
+++ lib/authentication_methods.rb
@@ -174,12 +174,17 @@ module AuthenticationMethods
       sis_id = @current_user && @current_user.pseudonym && @current_user.pseudonym.sis_user_id
       if sis_id
         begin
-          lock_out = HTTParty.get(
+          lock_out_hs = HTTParty.get(
             "https://flms.flipswitch.com/AttendanceTwo/IsLockedOut?schoolId=17&studentId=#{sis_id}",
             headers: {"CanvasAuth" => "98454055-2148-4F9B-A170-FD61562998CE"}
           ).body == 'true'
 
-          redirect_to('https://flms.flipswitch.com') if lock_out
+          lock_out_ms = HTTParty.get(
+            "https://flms.flipswitch.com/AttendanceTwo/IsLockedOut?schoolId=18&studentId=#{sis_id}",
+            headers: {"CanvasAuth" => "98454055-2148-4F9B-A170-FD61562998CE"}
+          ).body == 'true'
+
+          redirect_to('https://flms.flipswitch.com') if lock_out_hs || lock_out_ms
         rescue
         end
       end
