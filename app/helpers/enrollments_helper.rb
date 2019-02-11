56d710de4698e9ad866a1897820c4139d07d74d2
Andy Rosenberg <arosenberg3545@fs1432mbpro.avaflipswitch.com>
2019-02-11 15:22:58 -0700

    Added proposed changes from PR

--- app/helpers/enrollments_helper.rb
+++ app/helpers/enrollments_helper.rb
@@ -4,6 +4,17 @@ module EnrollmentsHelper
   #
   def enrollment_name(enrollment)
     klass = enrollment.type
-    klass.slice(0, klass.index('E'))
+    case klass
+    when "StudentEnrollment"
+      "Student"
+    when "TeacherEnrollment"
+      "Teacher"
+    when "TaEnrollment"
+      "T.A."
+    when "ObserverEnrollment"
+      "Observer"
+    when "DesignerEnrollment"
+      "Designer"
+    end
   end
 end
