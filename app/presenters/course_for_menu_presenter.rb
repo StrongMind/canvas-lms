19363447dfc6684da46aa1aea5ad24c558a8313f
Tesla Team <tesla@strongmind.com>
2019-04-05 13:56:39 -0700

    Another nil guard

--- app/presenters/course_for_menu_presenter.rb
+++ app/presenters/course_for_menu_presenter.rb
@@ -35,7 +35,7 @@ class CourseForMenuPresenter
   attr_reader :course, :available_section_tabs
 
   def to_h
-    current_requirement = CourseProgress.new(course, @user).current_content_tag.try(:id)
+    current_requirement = CourseProgress.new(course, @user).try(:current_content_tag).try(:id)
     resume_link = current_requirement ? current_requirement : course_path(course, :invitation => course.read_attribute(:invitation))
 
     {
