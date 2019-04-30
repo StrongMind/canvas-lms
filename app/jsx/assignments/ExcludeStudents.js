09e21ae30464a79d9ea9a61095230d17b7640a36
Chris Young <beesucker@gmail.com>
2019-04-30 12:10:11 -0700

    use a hash to do lookups

--- app/jsx/assignments/ExcludeStudents.js
+++ app/jsx/assignments/ExcludeStudents.js
@@ -4,15 +4,45 @@ class ExcludeStudents extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            students: ['Chris Young']
+            students: 
+                {
+                    1: {id: 1, name: 'Chris Young'},
+                    2: {id: 2, name: 'Andy Rosenberg'},
+                    3: {id: 3, name: 'John VanBuskirk'}
+                }
+            ,
+            exceptions: { 2: {id: 2, name: 'Andy Rosenberg'} }
         }
+        this.handleStudentClick = this.handleStudentClick.bind(this)
+    }
+    
+    renderStudentListItem(item) {
+        return(
+            <li>
+                <input type="checkbox" data-student-id={item.id} checked={this.state.exceptions[item.id]} onClick={this.handleStudentClick}></input>
+                &nbsp;
+                <span>{item.name}</span>
+            </li> 
+        )
+    }
+
+    renderStudentList () {
+        return (
+            Object.keys(this.state.students).map((key) => 
+                this.renderStudentListItem(this.state.students[key])
+            )
+        )
+    }
+
+    handleStudentClick(e) {
+        console.log(e.target.getAttribute('data-student-id'))
     }
   
     render() {
         return(    
-            <div>
-                { this.state.students.map((item,key) => <div>{item}</div>) }
-            </div>
+            <ul>
+                { this.renderStudentList() }
+            </ul>
         )
     }
 }
