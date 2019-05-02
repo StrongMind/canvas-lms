7c279f1e53f3d9c858d86fc9f7a95eb9e24172b3
Andy Rosenberg <andy.rosenberg@strongmind.com>
2019-05-02 14:31:09 -0700

    Take methods out of LMS controller

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -574,16 +574,4 @@ class AssignmentsController < ApplicationController
     (@assignment.vericite_enabled? && @context.vericite_pledge) ||
     @assignment.course.account.closest_turnitin_pledge
   end
-
-  def tiny_student_hash(group)
-    group.map {|obj| {id: obj.user_id, name: obj.user.name} }
-  end
-
-  def students_in_course
-    tiny_student_hash(@context.student_enrollments.where(type: "StudentEnrollment"))
-  end
-
-  def excused_students
-    tiny_student_hash(@assignment.submissions.where(excused: true))
-  end
 end
