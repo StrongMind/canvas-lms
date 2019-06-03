41c4b4cd24a71256beb16aee84b0816f7e3a94f9
Chris Irish <irish@velocitylabs.io>
2019-06-03 16:22:39 -0700

    spec updates and shim hash bump

--- spec_strongmind/features/custom_placement/custom_placement_permissions_spec.rb
+++ spec_strongmind/features/custom_placement/custom_placement_permissions_spec.rb
@@ -1,7 +1,7 @@
 
 require_relative '../../rails_helper'
 
-RSpec.describe 'As a Teacher I can NOT force advance a PENDING student', type: :feature, js: true do
+RSpec.describe 'As a Teacher using custom placement', type: :feature, js: true do
 
   include_context 'stubbed_network'
 
@@ -12,10 +12,34 @@ RSpec.describe 'As a Teacher I can NOT force advance a PENDING student', type: :
 
     @student = @student.reload
 
-    expect(@student.enrollments.first.workflow_state).to eq('invited')
+    expect(@student.enrollments.first).to be_invited
+
+    # Module 1 -------
+
+      @module1 = @course.context_modules.create!(:name => "Module 1")
+
+    # External Url
+      @external_url_tag = @module1.add_item(type: 'external_url', url: 'http://example.com/lolcats', title: 'External Url: requires viewing')
+      @external_url_tag.publish
+
+    # Context Module Sub Header
+      @header_tag = @module1.add_item(:type => "sub_header", :title => "Context Module Sub Header")
+
+    # Wiki Page or Page
+      wiki     = @course.wiki_pages.create! :title => "Wiki Page"
+      wiki_tag = @module1.add_item(:id => wiki.id, :type => 'wiki_page', :title => 'Wiki Page: requires viewing')
+
+    @module1.completion_requirements = {
+      @external_url_tag.id      => { type: 'must_view' },
+      wiki_tag.id               => { type: 'must_view' }
+    }
+
+    @module1.save!
+
+    Delayed::Testing.drain
   end
 
-  it "pending students do not get the Edit Enrollment link in their manage dropdown options" do
+  it "pending enrollment students get auto accepted" do
     visit "/courses/#{@course.id}"
 
     expect(page).to have_selector('a.home.active')
@@ -25,7 +49,24 @@ RSpec.describe 'As a Teacher I can NOT force advance a PENDING student', type: :
     within find("#user_#{@student.id}") do
       find('.al-trigger').click()
       sleep 1
-      expect(page).not_to have_link('Initial Placement')
+      click_link 'Initial Placement'
     end
+
+    expect(page).to have_selector('.ui-dialog', visible: true)
+
+    select "Wiki Page: requires viewing", from: 'Unit to start'
+
+    within '.ui-dialog' do
+      click_button 'Update'
+    end
+
+    accept_confirm
+
+    sleep 2
+
+    expect(page).to have_selector('.ic-flash-success', text: 'Custom placement process started. You can check progress by viewing the course as the student.')
+
+    @student = @student.reload
+    expect(@student.enrollments.first).to be_active
   end
 end
