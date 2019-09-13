d9eaed13479a2bbb590e424f7f7c3c9f118ac85d
Jessica Swanson <jessica.swanson@strongmind.com>
2019-09-13 15:30:43 -0700

    Change row style if assignment is excused

--- app/jsx/bundles/strongmind_gradebook.js
+++ app/jsx/bundles/strongmind_gradebook.js
@@ -1,2 +1,2 @@
-import '../strongmind_gradebook/main.c34805de.js'
-import '../strongmind_gradebook/main.c5792bcd.css'
+import '../strongmind_gradebook/main.5db4f677.js'
+import '../strongmind_gradebook/main.2ce17daf.css'
