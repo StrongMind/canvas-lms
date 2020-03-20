3c046da2c5552e9616814f88590c530c93f777ae
Andy Rosenberg <andy.rosenberg@strongmind.com>
2020-03-20 13:27:37 -0700

    Start to pull in data

--- app/jsx/observers/ObserveeCard.js
+++ app/jsx/observers/ObserveeCard.js
@@ -1,14 +1,25 @@
 import React from 'react'
 
 class ObserveeCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: this.props.student.attributes.user,
+      enrollments: this.props.student.attributes.enrollments
+    }
+  }
+
   static defaultProps = {
     student: {},
   };
 
   render() {
     return (
-      <div className="observee-card">
-        <p>I am being observed! #NoInterruptions</p>
+      <div
+       className="observee-card content-box pad-box-mini border border-trbl border-round-b"
+       style={{display: 'inline-block', margin: '1rem'}} >
+        <p>{this.state.user.name}</p>
       </div>
     )
   }
