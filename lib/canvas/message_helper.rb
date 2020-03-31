af2aa4193653f1dd9de3945d8255f8cc755b1f82
John Vanbuskirk <jvanbuskirk@strongmind.com>
2020-03-31 10:26:25 -0700

    fix comment

--- lib/canvas/message_helper.rb
+++ lib/canvas/message_helper.rb
@@ -70,7 +70,7 @@ module Canvas::MessageHelper
     using = { :delay_for => 0 }.with_indifferent_access.merge(values)
     using[:category] ||= args[1] # type
 
-    //bypass broken summaries and save 15% for spending 15 minutes 
+    #bypass broken summaries and save 15% for spending 15 minutes 
     return if using[:category] == 'Summaries'
 
     using[:delay_for] ||= args[2] # delay
