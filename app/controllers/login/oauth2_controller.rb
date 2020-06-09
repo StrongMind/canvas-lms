da50411b132c7245b42958a7e984c9ac20108ba8
Andy Rosenberg <andy.rosenberg@strongmind.com>
2020-06-09 12:43:50 -0700

    Take out settingservice log and only use email for provisioning logic

--- app/controllers/login/oauth2_controller.rb
+++ app/controllers/login/oauth2_controller.rb
@@ -39,7 +39,7 @@ class Login::Oauth2Controller < Login::OauthBaseController
       provider_attributes = @aac.provider_attributes(token)
 
       if SettingsService.get_settings(object: 'school', id: 1)['identity_server_enabled'] && @aac&.admin_role?(token)
-        unique_id = @aac.identity_email_address(token) || unique_id
+        unique_id = @aac.identity_email_address(token)
         provider_attributes["is_admin"] = true
       end
     end
