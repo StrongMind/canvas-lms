9758e287d8936ab6ecd7355620aa216352cfa42a
Andy Rosenberg <andy.rosenberg@strongmind.com>
2020-06-10 12:56:13 -0700

    Whoopsie

--- Gemfile.d/test.rb
+++ Gemfile.d/test.rb
@@ -16,8 +16,8 @@
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 
 group :test do
-  gem 'capybara',
-  gem 'capybara-screenshot',
+  gem 'capybara'
+  gem 'capybara-screenshot'
   gem 'climate_control',      '~> 0.2.0'
   gem 'database_cleaner',     '~> 1.7.0'
   gem 'factory_bot_rails',    '~> 4.11.1'
