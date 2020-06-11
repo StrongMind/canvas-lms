3b33fc447c11f9f0afc11209a442f5b91bab763c
Andy Rosenberg <andy.rosenberg@strongmind.com>
2020-06-11 16:06:55 -0700

    Put the spec back

--- spec_strongmind/features/custom_placement/custom_placement_feature_flagged_spec.rb
+++ spec_strongmind/features/custom_placement/custom_placement_feature_flagged_spec.rb
@@ -10,6 +10,22 @@ RSpec.describe 'As a System with custom placement behind feature flag', type: :f
     course_with_teacher_logged_in(course: @course)
   end
 
+  it "should be off by default" do
+    visit "/courses/#{@course.id}"
+    allow(SettingsService).to receive(:get_settings).and_return({})
+    allow(@teacher.teacher_enrollments.last).to receive(:has_permission_to?).with(:read_roster).and_return(true)
+    allow(@teacher.teacher_enrollments.last).to receive(:has_permission_to?).with(:manage_grades).and_return(true)
+    allow(@course.teacher_enrollments.last).to receive(:has_permission_to?).with(:custom_placement).and_return(false)
+
+    click_link 'People'
+
+    within find("#user_#{@student.id}") do
+      find('.al-trigger').click()
+      sleep 2
+      expect(page).not_to have_selector('a[href="#"][data-event=editEnrollments]', text: 'Custom Placement')
+    end
+  end
+
   it "can be turned on with a 'enable_custom_placement' feature flag" do
     visit "/courses/#{@course.id}"
 
