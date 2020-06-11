c51df046a3184eeac90d762a7fdd68b52b6dcd9a
Andy Rosenberg <andy.rosenberg@strongmind.com>
2020-06-11 15:01:52 -0700

    Put it back down to 2

--- spec_strongmind/features/custom_placement/custom_placement_feature_flagged_spec.rb
+++ spec_strongmind/features/custom_placement/custom_placement_feature_flagged_spec.rb
@@ -5,27 +5,7 @@ RSpec.describe 'As a System with custom placement behind feature flag', type: :f
 
   include_context 'stubbed_network'
 
-  before(:each) do
-    student_in_course(active_all: true)
-    course_with_teacher_logged_in(course: @course)
-  end
-
-  it "should be off by default and can be turned on with a 'enable_custom_placement' feature flag" do
-    visit "/courses/#{@course.id}"
-    allow(@teacher.teacher_enrollments.last).to receive(:has_permission_to?).with(:read_roster).and_return(true)
-    allow(@teacher.teacher_enrollments.last).to receive(:has_permission_to?).with(:manage_grades).and_return(true)
-    allow(@teacher.teacher_enrollments.last).to receive(:has_permission_to?).with(:custom_placement).and_return(false)
-
-    click_link 'People'
-
-    within find("#user_#{@student.id}") do
-      find('.al-trigger').click()
-      sleep 2
-      expect(page).not_to have_selector('a[href="#"][data-event=editEnrollments]', text: 'Custom Placement')
-    end
-  end
-
-  it "should be off by default and can be turned on with a 'enable_custom_placement' feature flag" do
+  it "can be turned on with a 'enable_custom_placement' feature flag" do
     visit "/courses/#{@course.id}"
 
     allow_any_instance_of(TeacherEnrollment).to receive(:has_permission_to?).and_return(true)
--- spec_strongmind/support/capybara.rb
+++ spec_strongmind/support/capybara.rb
@@ -119,7 +119,7 @@ end
 Capybara.configure do |config|
   config.server                = :thin # :strongmind/:puma/:webrick
   config.javascript_driver     = ENV['HEADLESS'] ? :headless_chrome : :chrome
-  config.default_max_wait_time = 100
+  config.default_max_wait_time = 15
 end
 
 # better looking screenshots (fixes relative paths when desired)
