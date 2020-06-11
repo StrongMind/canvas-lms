09ec2fe3e064a9153d1acadb973df7bcfe3f5740
Andy Rosenberg <andy.rosenberg@strongmind.com>
2020-06-11 14:29:34 -0700

    On and off states

--- spec_strongmind/features/custom_placement/custom_placement_feature_flagged_spec.rb
+++ spec_strongmind/features/custom_placement/custom_placement_feature_flagged_spec.rb
@@ -12,9 +12,23 @@ RSpec.describe 'As a System with custom placement behind feature flag', type: :f
 
   it "should be off by default and can be turned on with a 'enable_custom_placement' feature flag" do
     visit "/courses/#{@course.id}"
+    allow(@teacher.teacher_enrollments.last).to receive(:has_permission_to?).with(:read_roster).and_return(true)
+    allow(@teacher.teacher_enrollments.last).to receive(:has_permission_to?).with(:manage_grades).and_return(true)
+    allow(@teacher.teacher_enrollments.last).to receive(:has_permission_to?).with(:custom_placement).and_return(false)
+
+    click_link 'People'
+
+    within find("#user_#{@student.id}") do
+      find('.al-trigger').click()
+      sleep 2
+      expect(page).not_to have_selector('a[href="#"][data-event=editEnrollments]', text: 'Custom Placement')
+    end
+  end
+
+  it "should be off by default and can be turned on with a 'enable_custom_placement' feature flag" do
+    visit "/courses/#{@course.id}"
 
     allow_any_instance_of(TeacherEnrollment).to receive(:has_permission_to?).and_return(true)
-    allow_any_instance_of(TaEnrollment).to receive(:has_permission_to?).and_return(true)
 
     click_link 'People'
 
