4e004185e5b74cc4cbfe1d9fe7813d6b6e969943
Andy Rosenberg <andy.rosenberg@strongmind.com>
2020-06-16 10:49:28 -0700

    Real specs with real stuff

---
+++ spec_strongmind/models/submission_observer_enrollment_spec.rb
@@ -0,0 +1,27 @@
+
+require_relative '../rails_helper'
+
+RSpec.describe Submission do
+  include_context 'stubbed_network'
+
+  let(:observer) { User.create }
+
+  let(:student_enrollment) do
+    StudentEnrollment.create!(user: student, course: @course)
+  end
+
+  before do
+    course_with_student_submissions(:active_all => true)
+    @course.enroll_user(observer, "ObserverEnrollment", {:allow_multiple_enrollments => true, :associated_user_id => @student.id})
+    @submission = @course.submissions.find_by(user: @student)
+  end
+
+  it "returns false on an unconcluded observer enrollment" do
+    expect(@submission.send(:user_is_observer?, observer)).to be false
+  end
+
+  it "returns true when observer enrollment is concluded" do
+    @course.enrollments.each(&:conclude)
+    expect(@submission.send(:user_is_observer?, observer)).to be true
+  end
+end
