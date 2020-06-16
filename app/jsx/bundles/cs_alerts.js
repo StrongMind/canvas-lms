752daa8bc6fac7e95b865096f40d48964cda3be0
Andy Rosenberg <andy.rosenberg@strongmind.com>
2020-06-16 14:53:49 -0700

    Try this way

--- app/jsx/bundles/cs_alerts.js
+++ app/jsx/bundles/cs_alerts.js
@@ -1,1 +1,14 @@
-console.log("I AM THE ONE WHO SHIMS")
+import $ from 'jquery'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import CSAlerts from 'jsx/cs_alerts/CSAlerts'
+import axios from 'axios'
+
+const $el = document.getElementById('cs-alerts-container')
+
+ReactDOM.render(
+  <CSAlerts
+    alerts={[]}
+  />,
+  $el
+)
