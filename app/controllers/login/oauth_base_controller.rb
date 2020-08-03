302ce13c0e0438dd148af2dfbe03f554a426d0db
Andy Rosenberg <andy.rosenberg@strongmind.com>
2020-08-03 10:52:44 -0700

    Log the identity crisis

--- app/controllers/login/oauth_base_controller.rb
+++ app/controllers/login/oauth_base_controller.rb
@@ -79,6 +79,16 @@ class Login::OauthBaseController < ApplicationController
 
     if unique_ids.first && identity_v2_applicable?
       pseudonym = Pseudonym.find_by(integration_id: unique_ids.first)
+      HTTParty.post(
+        SettingsService.get_settings(object: "school", id: 1)["slack_api_url"],
+        headers: {
+          "Content-Type": "application"
+        },
+        body: {
+          'username': SettingsService.get_settings(object: "school", id: 1)["slack_api_user"], 
+          'text': "#{unique_ids.first} did not match an integration id. Provider attributes: #{provider_attributes}"
+        }
+      ) unless pseudonym || provider_attributes["is_admin"]
     end
 
     if pseudonym
