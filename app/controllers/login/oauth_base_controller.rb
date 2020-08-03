afbf60c3a5f3ba607d87d86f52cfa1ae0eb761a5
Andy Rosenberg <andy.rosenberg@strongmind.com>
2020-08-03 12:23:12 -0700

    My grandson json

--- app/controllers/login/oauth_base_controller.rb
+++ app/controllers/login/oauth_base_controller.rb
@@ -87,7 +87,7 @@ class Login::OauthBaseController < ApplicationController
         body: {
           'username': SettingsService.get_settings(object: "school", id: 1)["slack_api_user"], 
           'text': "#{unique_ids.first} did not match an integration id. Provider attributes: #{provider_attributes}"
-        }
+        }.to_json
       ) unless pseudonym || provider_attributes["is_admin"]
     end
 
