e408a54b25f7f95c9efd96aeeb6383ffb983d34b
cpython1234 <charles.parisi@strongmind.com>
2022-08-16 13:03:08 -0700

    Set config.cache_classes = true to resolve login issue after enabling eager_loading on development environment.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,7 +21,7 @@ environment_configuration(defined?(config) && config) do |config|
   # In the development environment your application's code is reloaded on
   # every request.  This slows down response time but is perfect for development
   # since you don't have to restart the webserver when you make code changes.
-  config.cache_classes = false
+  config.cache_classes = true
 
   # Show full error reports and disable caching
   config.consider_all_requests_local = true
