c070221350a6f230b179b0ce6acb704d4a7b1821
Angel Diaz <angel.diaz@strongmind.com>
2022-12-20 10:54:39 -0700

    scripts: updated script for encrypting secrets for pulumi

--- script/encrypt_secret.rb
+++ script/encrypt_secret.rb
@@ -1,45 +1,41 @@
 #! /usr/bin/env ruby
+encryptable_keys =
+%w(
+    DB_PASS
+    ENCRYPTION_KEY
+    PIPELINE_PASSWORD
+    S3_ACCESS_KEY_ID
+    S3_ACCESS_KEY
+    OUTGOING_MAIL_PASSWORD
+    LAUNCH_DARKLY_SDK_KEY
+    CANVAS_LMS_ADMIN_PASSWORD
+  )
 
-class EncryptSecret
+  
+config_dir = File.join(Dir.getwd, 'infrastructure', 'partners')
 
-  def initialize(file)
-    @file = file
-  end
+file = ARGV[0]
+partner = ARGV[1]
 
-  def parse
-    File.open(@file) do |file|
-      file.each_line do |line|
+File.open(file) do |file|
+  file.each_line do |line|
 
-        key_value = line.split('=')
-        key, value = key_value[0], key_value[1]
-        next if value.nil?
-        if encryptable_keys.include?(key)
-          Dir.chdir(config_dir){
-            system("`pulumi config set  --secret #{key} #{value}`")
-          }
-        else
-          Dir.chdir(config_dir){
-            system("`pulumi config set #{key} #{value}`")
-          }
-        end
+    key_value = line.split('=')
+    key, value = key_value[0], key_value[1]
+    next if value.nil?
+    Dir.chdir(config_dir){
+      system("`pulumi stack select #{partner}`")
+
+      if encryptable_keys.include?(key)
+          system("`pulumi config set  --secret #{key} #{value}`")
+      else
+          system("`pulumi config set #{key} #{value}`")
       end
-    end
-  end
-
-  def encryptable_keys
-    %w(
-        DB_PASS
-        ENCRYPTION_KEY
-        PIPELINE_PASSWORD
-        S3_ACCESS_KEY_ID
-        S3_ACCESS_KEY
-        OUTGOING_MAIL_PASSWORD
-        LAUNCH_DARKLY_SDK_KEY
-        CANVAS_LMS_ADMIN_PASSWORD
-      )
-  end
-
-  def config_dir
-    File.join(Rails.root, 'infrastructure')
+    }
   end
 end
+
+
+
+
+
