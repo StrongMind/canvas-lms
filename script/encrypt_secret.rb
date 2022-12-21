fed3cfd1fd927310fad53a75294d8d322d52408d
Angel Diaz <angel.diaz@strongmind.com>
2022-12-20 20:40:56 -0700

    scripts: updated script to include partner name when chdir
    Co-authored-by: Christopher Adeszko <christopher.adeszko@strongmind.com>
    Co-authored-by: James Wingert <james.wingert@strongmind.com>

--- script/encrypt_secret.rb
+++ script/encrypt_secret.rb
@@ -9,13 +9,26 @@ encryptable_keys =
     OUTGOING_MAIL_PASSWORD
     LAUNCH_DARKLY_SDK_KEY
     CANVAS_LMS_ADMIN_PASSWORD
+    AIRBRAKE_PROJECT_KEY
+    ATTENDANCE_API_KEY_V2
+    DB_HOST
+    DB_USER
+    LAUNCH_DARKLY_API_KEY
+    OUTGOING_MAIL_USER_NAME
+    REDIS_SERVER
+    SIS_ENROLLMENT_UPDATE_API_KEY
+    STRONGMIND_INTEGRATION_KEY
+    TOPIC_MICROSERVICE_API_KEY
+    MASTER_PASSWORD
+    MASTER_USERNAME
   )
 
   
-config_dir = File.join(Dir.getwd, 'infrastructure', 'partners')
 
 file = ARGV[0]
 partner = ARGV[1]
+
+config_dir = File.join(Dir.getwd, 'infrastructure', 'partners', partner)
 
 File.open(file) do |file|
   file.each_line do |line|
@@ -24,7 +37,7 @@ File.open(file) do |file|
     key, value = key_value[0], key_value[1]
     next if value.nil?
     Dir.chdir(config_dir){
-      system("`pulumi stack select #{partner}`")
+      system("`pulumi stack select dev`")
 
       if encryptable_keys.include?(key)
           system("`pulumi config set  --secret #{key} #{value}`")
